4869530699c9604873b191c004ead4a0b08e1489
Thomas Sampson <tmsampson@gmail.com>
2021-10-10 15:39:42 +0100

    Setup challenge framework

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace aoc
+namespace AdventOfCode2020
 {
-	class Program
+	partial class Program
 	{
 		static void Main(string[] args)
 		{
-			
+			if(args.Length == 0)
+			{
+				Console.WriteLine("No arguments passed, running last challenge");
+				Challenges.Last();
+			}
+			else if(int.TryParse(args[0], out int challengeIndex) && challengeIndex > 0 && challengeIndex <= Challenges.Count)
+			{
+				Console.WriteLine($"Running challenge #{challengeIndex}");
+				Challenges[challengeIndex - 1]();
+			}
 		}
+
+		// Challenge list
+		static List<Action> Challenges = new List<Action>()
+		{
+			Challenge01
+		};
 	}
 }
