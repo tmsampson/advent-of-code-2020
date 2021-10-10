ffc8e05dfe6e9edd98283a7e347db485e463a75d
Thomas Sampson <tmsampson@gmail.com>
2021-10-10 15:39:28 +0100

    Setup challenge framework

---
+++ challenges/challenge-01.cs
@@ -0,0 +1,14 @@
+// https://adventofcode.com/2020/day/1
+
+using System;
+
+namespace AdventOfCode2020
+{
+	partial class Program
+	{
+		static void Challenge01()
+		{
+			Console.WriteLine("Challenge #1 Executed");
+		}
+	}
+}
